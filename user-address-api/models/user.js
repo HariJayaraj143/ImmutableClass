390688efda2caee29cc6b1ff06fcafd71424331a
Harish <nxtwave@ip-10-2-0-167.ap-south-1.compute.internal>
2024-09-30 11:52:14 +0000

    Usersapi

--- user-address-api/models/user.js
+++ user-address-api/models/user.js
@@ -1,11 +1,11 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('./index');
+const {DataTypes} = require('sequelize')
+const sequelize = require('./index')
 
 const User = sequelize.define('User', {
   name: {
     type: DataTypes.STRING,
     allowNull: false,
   },
-});
+})
 
-module.exports = User;
+module.exports = User
